1976e4d340914957d8ab13181a363c60b819a0f9
LiudmilaLi <LiudmilaLitvin@gmail.com>
2019-10-16 00:02:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 === 1) { 
+        return false; }
+            
+    const brackets = [];
+    for (const elem of bracketsConfig) {
+       if (elem[0] !== elem[1]) {
+        brackets[elem[0]] = elem[1];
+        } else { brackets[elem[0]] = ' '; }
+    }
+    const string = []; 
+    for (let i = 0; i < str.length; i++) {
+        const currentElem = str[i];
+        if (brackets[currentElem]) {
+          if (brackets[currentElem] !== ' ' || string[string.length - 1] !== currentElem) {
+          string.push(currentElem); 
+        } else { 
+          string.pop(); }
+          continue;
+      }
+      if (brackets[string[string.length - 1]] === currentElem) {
+        string.pop(); 
+        } else { 
+          return false; }
+    }
+  return string.length === 0;
+    }
